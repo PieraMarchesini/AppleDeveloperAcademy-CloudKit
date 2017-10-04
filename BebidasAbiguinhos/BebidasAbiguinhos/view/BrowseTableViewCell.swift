a17fa258c9259cd756cff79da9e340070216edaa
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-10-04 17:41:08 -0300

    Storyboard added

---
+++ BebidasAbiguinhos/BebidasAbiguinhos/view/BrowseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrowseTableViewCell.swift
+//  BebidasAbiguinhos
+//
+//  Created by Piera Marchesini on 04/10/17.
+//  Copyright © 2017 Piera Marchesini. All rights reserved.
+//
+
+import UIKit
+
+class BrowseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
